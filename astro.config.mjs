8d3ef63845822ad783b925335548e8ae9a01221d
Morris Liu <morrisliu1994@outlook.com>
2025-11-20 14:04:50 +0800

    fix build issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,15 @@ import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://realmorrisliu.com",
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   trailingSlash: "never",
   build: {
     format: "file",
