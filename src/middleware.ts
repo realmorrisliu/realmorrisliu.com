496379d281c6fef2b75ffbede43685ad1b2e611b
Morris Liu <morrisliu1994@outlook.com>
2025-11-20 17:27:14 +0800

    add protected route, kira

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,11 @@
-import { clerkMiddleware } from "@clerk/astro/server";
-import { defineMiddleware } from "astro/middleware";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
-export const onRequest = defineMiddleware((context, next) => {
-  return clerkMiddleware()(context, () => {
-    if (typeof context.locals.auth !== "function") {
-      context.locals.auth = () =>
-        ({
-          userId: null,
-          sessionId: null,
-          getToken: async () => null,
-          has: () => false,
-          debug: () => {},
-          redirectToSignIn: async () => {},
-        }) as any;
-    }
-    return next();
-  });
+const isProtectedRoute = createRouteMatcher(["/kira(.*)"]);
+
+export const onRequest = clerkMiddleware((auth, context) => {
+  const { userId, redirectToSignIn } = auth();
+
+  if (!userId && isProtectedRoute(context.request)) {
+    return redirectToSignIn();
+  }
 });
